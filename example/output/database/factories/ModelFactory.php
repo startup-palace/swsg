e20c92b98e0b6ffebdfcf776ae0264cf34ab825d
David Sferruzza <david.sferruzza@gmail.com>
2017-08-02 18:03:33 +0200

    Remove useless stuff

--- example/output/database/factories/ModelFactory.php
+++ example/output/database/factories/ModelFactory.php
@@ -12,7 +12,7 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+/*$factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
@@ -22,3 +22,4 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+*/
