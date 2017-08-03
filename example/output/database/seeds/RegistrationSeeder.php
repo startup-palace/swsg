537c6175aa5162d4b67ef5e066674a833bf70c03
David Sferruzza <david.sferruzza@gmail.com>
2017-08-03 16:49:14 +0200

    Refactor a seeder

--- example/output/database/seeds/RegistrationSeeder.php
+++ example/output/database/seeds/RegistrationSeeder.php
@@ -13,8 +13,13 @@ class RegistrationSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('registration')->insert([
+        DB::table('registration')->insert(self::registrationData());
+    }
+
+    public static function registrationData() {
+        return [
             ['name' => 'Batman', 'email' => 'batman@wayne-corp.com', 'date' => Carbon::create(2017, 07, 01, 20, 00, 00, 'Europe/Paris')],
-        ]);
+            ['name' => 'Superman', 'email' => 'clark.kent@thedailyplanet.com', 'date' => Carbon::create(2017, 07, 8, 7,30, 00, 'Europe/Paris')],
+        ];
     }
 }
