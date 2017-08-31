3714f2c353de0ff4f34fc876e7d009fda9c6c004
David Sferruzza <david.sferruzza@gmail.com>
2017-08-31 16:39:33 +0200

    Update example to Laravel 5.5

--- example/output/config/app.php
+++ example/output/config/app.php
@@ -177,8 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         //App\Providers\RouteServiceProvider::class,
         App\Providers\GeneratedRouteServiceProvider::class,
-
-        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
