117fe42938258deb2a4ab67d4037c021c9f21c5a
David Sferruzza <david.sferruzza@gmail.com>
2017-11-07 23:27:32 -0500

    Improve the way path parameters are retrieved

--- jvm/src/main/twirl/swsg/backend/laravel/routes.scala.php
+++ jvm/src/main/twirl/swsg/backend/laravel/routes.scala.php
@@ -1,12 +1,13 @@
 @(cs: Set[swsg.Model.Component], services: Seq[swsg.Model.Service])<?php
 // @Backend.header
 
+use \Illuminate\Http\Request;
 use @{Laravel.swsgNamespace}\Ctx;
 use @{Laravel.swsgNamespace}\Params;
 
 @for(s <- services) {
-Route::match(['@{s.method.toLowerCase}'], '@s.path', function (@{s.params.map(p => s"$$${p.variable.name}").mkString(", ")}) {
-    $pathParams = [@{s.params.map(p => s"'${p.variable.name}' => $$${p.variable.name}").mkString(", ")}];
+Route::match(['@{s.method.toLowerCase}'], '@s.path', function (Request $req) {
+    $pathParams = [@{s.params.map(p => s"'${p.variable.name}' => $$req->route()->parameter('${p.variable.name}')").mkString(", ")}];
     $initialContext = new Ctx($pathParams);
     return @{Laravel.instantiate(cs, s.component, "$initialContext")};
 });
