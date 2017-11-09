b69d6d7a8ae8f30932fddd99d2d26c0cedee0d17
David Sferruzza <david.sferruzza@gmail.com>
2017-11-09 16:33:22 -0500

    Handle several kinds of service parameters

--- jvm/src/main/twirl/swsg/backend/laravel/routes.scala.php
+++ jvm/src/main/twirl/swsg/backend/laravel/routes.scala.php
@@ -7,8 +7,11 @@ use @{Laravel.swsgNamespace}\Params;
 
 @for(s <- services) {
 Route::match(['@{s.method.toLowerCase}'], '@s.path', function (Request $req) {
-    $pathParams = [@{s.params.map(p => s"'${p.variable.name}' => $$req->route()->parameter('${p.variable.name}')").mkString(", ")}];
-    $initialContext = new Ctx($pathParams);
+    $queryParams = [@{s.params.filter(_.location == swsg.Model.Query).map(p => s"'${p.variable.name}' => $$req->query('${p.variable.name}')").mkString(", ")}];
+    $headerParams = [@{s.params.filter(_.location == swsg.Model.Header).map(p => s"'${p.variable.name}' => $$req->header('${p.variable.name}')").mkString(", ")}];
+    $pathParams = [@{s.params.filter(_.location == swsg.Model.Path).map(p => s"'${p.variable.name}' => $$req->route()->parameter('${p.variable.name}')").mkString(", ")}];
+    $cookieParams = [@{s.params.filter(_.location == swsg.Model.Cookie).map(p => s"'${p.variable.name}' => $$req->cookie('${p.variable.name}')").mkString(", ")}];
+    $initialContext = new Ctx(array_merge($queryParams, $headerParams, $pathParams, $cookieParams));
     return @{Laravel.instantiate(cs, s.component, "$initialContext")};
 });
 }
