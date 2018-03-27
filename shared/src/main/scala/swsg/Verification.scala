938080e49f642ed04271220bc538fdd1284fb11c
David Sferruzza <david.sferruzza@gmail.com>
2018-03-27 15:43:10 +0200

    Refactor verifications hierarchy

--- shared/src/main/scala/swsg/Verification.scala
+++ shared/src/main/scala/swsg/Verification.scala
@@ -19,31 +19,27 @@ final case object ConsistencyVerification extends AutoVerification {
 
   val levels = Seq(
     Set(
-      ComponentNameUnicity, // Component name uniqueness
-      EntityNameUnicity, // Entity name uniqueness
-      AttributeNameUnicity, // Same entity’s attributes name uniqueness
-      ServiceParameterNameUnicity,
-      ServiceBodyUnicity,
-      ReferenceConsistency
-    ),
-    Set(
-      RecursiveReferenceConsistency, // Composite components flattenability
-      ComponentContextVariableNameUnicity, // Context variable name uniqueness
+      ComponentNameUnicity, // Component name unicity
+      EntityNameUnicity, // Entity name unicity
+      AttributeNameUnicity, // Attribute name unicity
+      ServiceParameterNameUnicity, // Service parameter name unicity
+      ServiceBodyUnicity, // Parameters of location body unicity
+      ReferenceConsistency, // Reference consistency
+      ComponentContextVariableNameUnicity, // Component context variable name unicity
       CompositeComponentNonEmptiness, // Composite component nonemptiness
-      ContextVariablesTypeValidity,
-      AliasSourceUnicity,
-      AliasTargetUnicity,
-      AliasSourceValidity,
-      AliasTargetValidity,
-      ServicePathValidity
+      ContextVariablesTypeValidity, // **Implementation details**
+      AliasSourceUnicity, // Alias source unicity
+      AliasTargetUnicity // Alias target unicity
     ),
     Set(
-      ComponentContextImmutability, // Context immutability
-      ComponentPreconditionExhaustivity, // Component’s precondition exhaustivity
-      ComponentInstanceBindingsConsistency
-    ),
-    Set(
-      ComponentInstanceParametersExhaustivity, // Component instances’ parameters exhaustivity
+      RecursiveReferenceConsistency, // Recursive reference consistency
+      AliasSourceValidity, // Alias source validity
+      AliasTargetValidity, // Alias target validity
+      ServicePathValidity, // Service path validity
+      ComponentContextImmutability, // Component context immutability
+      ComponentPreconditionExhaustivity, // Component precondition exhaustivity
+      ComponentInstanceBindingsConsistency, // Component instance bindings consistency
+      ComponentInstanceParametersExhaustivity, // Component instance parameters exhaustivity
       ContextValidity // Context validity
     )
   )
